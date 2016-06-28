089a0c2b4179658789679173432a8844df8ae762
dodanhhieu <hieudd@atmarkcafe.org>
2016-06-28 09:26:01 +0700

    init test

---
+++ Mantest/Mantest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mantest
+//
+//  Created by macmini04 on 6/28/16.
+//  Copyright © 2016 dodanhhieu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
